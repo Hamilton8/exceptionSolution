f5001a0c6468b02e2632c8299ac1ef63b1aec871
Hamilton8 <103421686+Hamilton8@users.noreply.github.com>
2025-02-23 10:53:18 +0530

    Update DomainException.java
    some details modified from the remote repository

--- src/model/exception/DomainException.java
+++ src/model/exception/DomainException.java
@@ -1,6 +1,11 @@
 package src.model.exception;
 
-public class DomainException extends Exception{
+public class DomainException extends RuntimeException{
+
+
+    public DomainException(){
+        super();
+    }
 
     public DomainException(String mess){
         super(mess);
